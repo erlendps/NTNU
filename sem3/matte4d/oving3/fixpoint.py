8c34604d3985b2823f8fdb88d218de971c9f6acd
Erlend Skaaden <erlendskaaden@gmail.com>
2021-09-23 15:20:02 +0200

    add fixpoint python program

---
+++ sem3/matte4d/oving3/fixpoint.py
@@ -0,0 +1,13 @@
+import math as ma
+
+def fixpoint(function, initial_value, iteration):
+    if (iteration == 0):
+        return initial_value
+    else:
+        return function(fixpoint(function, initial_value, iteration-1))
+
+def g(x):
+    return x - ma.sin(x) + 1
+
+print(fixpoint(g, ma.pi/4, 200))
+print(ma.pi/2)
